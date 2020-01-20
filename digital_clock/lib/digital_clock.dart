e856ea6ef31c8f261588e8c4f75b8d69cd0ab427
Yelizaveta Gnezdilova <Lizardy@users.noreply.github.com>
2020-01-21 03:34:00 +0800

    animate digits with a "growing up" motion: during 6 seconds "pixels" are being gradually lit from bottom to top and from small to big, repeating a cycle for 10 times = one minute long, then all over again

--- digital_clock/lib/digital_clock.dart
+++ digital_clock/lib/digital_clock.dart
@@ -19,7 +19,7 @@ final _lightTheme = {
   _Element.backgroundStartGradient: Colors.grey[50],
   _Element.backgroundEndGradient: Colors.grey[300],
   _Element.text: Colors.teal[700],
-  _Element.shadow: Colors.tealAccent[100],
+  _Element.shadow: Colors.tealAccent,
 };
 
 final _darkTheme = {
@@ -30,30 +30,31 @@ final _darkTheme = {
 };
 
 // bitmaps for each digit: 3 "pixels" horizontally X 5 "pixels" vertically
-const Map<String, List<List<bool>>> digitsBlueprints = {
-  '0': [[true, true, true], [true, false, true], [true, false, true], [true, false, true], [true, true, true]],
-  '1': [[false, true, false], [true, true, false], [false, true, false], [false, true, false], [false, true, false]],
-  '2': [[true, true, true], [false, false, true], [true, true, true], [true, false, false], [true, true, true]],
-  '3': [[true, true, true], [false, false, true], [true, true, true], [false, false, true], [true, true, true]],
-  '4': [[true, false, true], [true, false, true], [true, true, true], [false, false, true], [false, false, true]],
-  '5': [[true, true, true], [true, false, false], [true, true, true], [false, false, true], [true, true, true]],
-  '6': [[true, true, true], [true, false, false], [true, true, true], [true, false, true], [true, true, true]],
-  '7': [[true, true, true], [false, false, true], [false, true, false], [false, true, false], [false, true, false]],
-  '8': [[true, true, true], [true, false, true], [true, true, true], [true, false, true], [true, true, true]],
-  '9': [[true, true, true], [true, false, true], [true, true, true], [false, false, true], [true, true, true]],
+// '0'-'9': digits, 1-5 keys: row numbers, 0-5 values: pixel size multiplier
+const Map<String, Map<int, List<int>>> digitsBlueprints = {
+  '0': {1: [1, 1, 1], 2: [2, 0, 2], 3: [3, 0, 3], 4: [4, 0, 4], 5: [5, 5, 5]},
+  '1': {1: [0, 1, 0], 2: [2, 2, 0], 3: [0, 3, 0], 4: [0, 4, 0], 5: [0, 5, 0]},
+  '2': {1: [1, 1, 1], 2: [0, 0, 2], 3: [3, 3, 3], 4: [4, 0, 0], 5: [5, 5, 5]},
+  '3': {1: [1, 1, 1], 2: [0, 0, 2], 3: [3, 3, 3], 4: [0, 0, 4], 5: [5, 5, 5]},
+  '4': {1: [1, 0, 1], 2: [2, 0, 2], 3: [3, 3, 3], 4: [0, 0, 4], 5: [0, 0, 5]},
+  '5': {1: [1, 1, 1], 2: [2, 0, 0], 3: [3, 3, 3], 4: [0, 0, 4], 5: [5, 5, 5]},
+  '6': {1: [1, 1, 1], 2: [2, 0, 0], 3: [3, 3, 3], 4: [4, 0, 4], 5: [5, 5, 5]},
+  '7': {1: [1, 1, 1], 2: [0, 0, 2], 3: [0, 3, 0], 4: [0, 4, 0], 5: [0, 5, 0]},
+  '8': {1: [1, 1, 1], 2: [2, 0, 2], 3: [3, 3, 3], 4: [4, 0, 4], 5: [5, 5, 5]},
+  '9': {1: [1, 1, 1], 2: [2, 0, 2], 3: [3, 3, 3], 4: [0, 0, 4], 5: [5, 5, 5]},
 };
 
 const List<List<List<int>>> dividerBlueprints = [
   [
     [0, 0, 0],
-    [0, 2, 0],
+    [0, 1, 0],
     [0, 0, 0],
     [0, 4, 0],
     [0, 0, 0]
   ],
   [
     [0, 0, 0],
-    [0, 4, 0],
+    [0, 3, 0],
     [0, 0, 0],
     [0, 2, 0],
     [0, 0, 0]
@@ -61,30 +62,29 @@ const List<List<List<int>>> dividerBlueprints = [
 ];
 
 class DigitPixel extends StatelessWidget {
+  final num pixelSizeMax;
+  final num pixelSizeLit;
+  final bool backLit;
   final bool lit;
-  final int rowNumber;
-  DigitPixel(this.lit, this.rowNumber);
+
+  DigitPixel(this.pixelSizeMax, this.pixelSizeLit, this.backLit, this.lit);
 
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).brightness == Brightness.light
         ? _lightTheme
         : _darkTheme;
-    final pixelSizeMax = MediaQuery.of(context).size.width / 20;
-    final pixelSizeLit = pixelSizeMax / 5 * (rowNumber + 1.5);
     return Padding(
       padding: const EdgeInsets.all(0.5),
-      child: AnimatedContainer(
-        duration: Duration(seconds: 1),
-        curve: Curves.ease,
+      child: Container(
         width: pixelSizeMax,
         height: pixelSizeMax,
-        color: lit
+        color: backLit
             ? colors[_Element.shadow]
-            : colors[_Element.backgroundEndGradient].withOpacity(0.0),
+            : colors[_Element.shadow].withOpacity(0.0),
         alignment: Alignment(0, 0),
         child: AnimatedContainer(
-          duration: Duration(seconds: 2),
+          duration: Duration(seconds: 1),
           curve: Curves.ease,
           width: lit ? pixelSizeLit : 0,
           height: lit ? pixelSizeLit : 0,
@@ -95,23 +95,68 @@ class DigitPixel extends StatelessWidget {
   }
 }
 
-class DigitVisualization extends StatelessWidget {
+class DigitVisualization extends StatefulWidget {
   final String digit;
-  DigitVisualization(this.digit);
+  final int second;
+  DigitVisualization(this.digit, this.second);
+
+  @override
+  _DigitVisualizationState createState() => _DigitVisualizationState();
+}
+
+class _DigitVisualizationState extends State<DigitVisualization> {
+  Map<int, Map<int, List<int>>> digitAnimationBlueprints = {};
+  Map<int, List<int>> digitBlueprints;
+  final emptyRow = [0, 0, 0];
+  String oldDigit;
+  int step;
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  void init() {
+    oldDigit = widget.digit;
+    digitAnimationBlueprints = {};
+    digitBlueprints = digitsBlueprints.containsKey(widget.digit)
+        ? digitsBlueprints[widget.digit]
+        : throw Exception('digit\'s blueprint not found for ${widget.digit}');
+    // animation in 6 steps x 10 times during 1 minute
+    for (int step=0; step<6; step++) {
+      Map<int, List<int>> rows = {};
+      int rowNumber;
+      // 5-step empty/placeholder rows
+      for (rowNumber=1; rowNumber<=5-step; rowNumber++) {
+        rows[rowNumber] = emptyRow;
+      }
+      // the rest of the rows with a visible part of the "growing up" digit
+      for (int rowNumberVisibleDigit=step, rowNumber=5; rowNumberVisibleDigit>0; rowNumberVisibleDigit--, rowNumber--) {
+        rows[rowNumber] = digitBlueprints[rowNumber].map((e) => (e > 0) ? rowNumberVisibleDigit : 0).toList();
+      }
+      digitAnimationBlueprints[step] = rows;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<List<bool>> _blueprint = digitsBlueprints[digit];
+    if (oldDigit != widget.digit) init();
     List<List<Widget>> _rows = [];
-    int _index = 0;
-    _blueprint.forEach((List<bool> blueprintRow){
+    int step = widget.second - 6 * (widget.second / 6).floor();
+    num pixelSizeMax = MediaQuery.of(context).size.width / 20;
+    for (int j=1; j<=5; j++) {
       List<Widget> _row = [];
-      blueprintRow.forEach((bool pixelValue) {
-        _row.add(DigitPixel(pixelValue, _index));
-      });
-      _index++;
+      for (var k=0; k<3; k++) {
+        int pixelSizeMultiplier = digitAnimationBlueprints[step][j][k];
+        num pixelSizeLit = pixelSizeMax / 5 * (pixelSizeMultiplier + 0.5);
+        bool pixelBackLit = digitBlueprints[j][k] > 0 ? true : false;
+        bool pixelLit = digitBlueprints[j][k] > 0 && pixelSizeMultiplier > 0 ? true : false;
+        _row.add(DigitPixel(pixelSizeMax, pixelSizeLit, pixelBackLit, pixelLit));
+      }
       _rows.add(_row);
-    });
+    }
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
@@ -266,6 +311,7 @@ class _DigitalClockState extends State<DigitalClock> {
     final hour = DateFormat(widget.model.is24HourFormat ? 'HH' : 'hh')
         .format(_dateTime);
     final minute = DateFormat('mm').format(_dateTime);
+    final second = _dateTime.second;
     final isSecondEven = _dateTime.second % 2 == 0 ? true : false;
 
     return Container(
@@ -285,11 +331,11 @@ class _DigitalClockState extends State<DigitalClock> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                DigitVisualization(hour[0]),
-                DigitVisualization(hour[1]),
+                DigitVisualization(hour[0], second),
+                DigitVisualization(hour[1], second),
                 DividerVisualization(isSecondEven),
-                DigitVisualization(minute[0]),
-                DigitVisualization(minute[1]),
+                DigitVisualization(minute[0], second),
+                DigitVisualization(minute[1], second),
               ],
             ),
           ],
